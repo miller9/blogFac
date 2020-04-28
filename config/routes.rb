7889f0f84532f99a3f930a18a6c1c1070c343269
miller9 <millerpoli9@gmail.com>
2020-04-28 17:12:51 -0400

    crear root_path y modificar vista

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "bienvenida", to: "home#index"
 
+  root to: "home#index"
+
+
   get "articles/new", to: "articles#new"
   get "articles/:id", to: "articles#show" #using :id to get specific values inside SHOW
   get "articles/:id/edit", to: "articles#edit"
