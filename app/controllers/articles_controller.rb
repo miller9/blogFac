c38d57b04bfc1940e7a1e823df4ccc9b95717eca
miller9 <millerpoli9@gmail.com>
2020-04-28 16:57:43 -0400

    Crear funcion Delete, modificar vista y crear ruta

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,4 +27,11 @@ class ArticlesController < ApplicationController
     render json: @article
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   get "articles/:id", to: "articles#show" #using :id to get specific values inside SHOW
   get "articles/:id/edit", to: "articles#edit"
 
-  patch "/articles/:id", to: "articles#update", as: :article # nombre de la ruta
+  patch "articles/:id", to: "articles#update", as: :article # nombre de la ruta
   post "articles", to: "articles#create" # solves GET route issue..
+
+  delete "articles/:id", to: "articles#destroy"
+
 
 end
