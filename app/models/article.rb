ba67e9996ab6f2c63952d968df8907ba7db983ff
miller9 <millerpoli9@gmail.com>
2020-06-11 15:38:25 -0500

    DB_Associations2. Relacion 1a* entre articles/categories y has_categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   has_rich_text :content # Model has one rich text field
   belongs_to :user #campo adicional
+  has_many :has_categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  has_many :has_categories
 end
