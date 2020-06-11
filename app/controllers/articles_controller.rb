37437dd45e788aeba5e29fb39e9c3a9da97568c0
miller9 <millerpoli9@gmail.com>
2020-06-11 16:44:59 -0500

    DB_Associations2. Agrego nuevo campo 'category_elements al form del Modelo article. Actualizo StrongParams en articles_controller. Agrego 'attr_accessor' y funcion 'save_categories' en Modelo article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
 
   def update
     @article.update(article_params)
+    @article.save_categories # call function to save new 'category_elements'
     redirect_to @article
   end
 
@@ -25,6 +26,7 @@ class ArticlesController < ApplicationController
   # guardar lo recibido del formulario
   def create
     @article = current_user.articles.create(article_params)
+    @article.save_categories # call function to save new 'category_elements'
     redirect_to @article
   end
 
@@ -43,7 +45,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title,:content)
+    params.require(:article).permit(:title,:content,:category_elements)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,20 @@ class Article < ApplicationRecord
   belongs_to :user #campo adicional
   has_many :has_categories
   has_many :categories, through: :has_categories
+  attr_accessor :category_elements
+
+  def save_categories
+    # category_elements 1,2,3...
+    # Convertir a array 1,2,3 => [1,2,3]
+    categories_array = category_elements.split(",")
+    # Iterar el array
+    # Crear HasCategory, para establecer la relacion
+    #   HasCategory<article_id: 1, category_id: 2>
+    categories_array.each do |category_id|
+      HasCategory.create(article: self, category_id: category_id)
+    end
+
+
+  end
+
 end
