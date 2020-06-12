46ed21ff99398cadbf30cad5841b501aea87a915
miller9 <millerpoli9@gmail.com>
2020-06-11 21:32:31 -0500

    ActiveStorage. Creo controlador Users y funcion EDIT

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  # get 'users/edit' ruta innecesaria creada por generador
   devise_for :users
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
